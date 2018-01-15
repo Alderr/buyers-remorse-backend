e976d9766f2c66d599dd5c7c737ebc7a19e5e091
Vernon Mensah <vernonmensah@gmail.com>
2018-01-15 18:28:29 -0500

    Able to get currentRate and pastRate with hard coded a fixed coin/date/exchange

--- server.js
+++ server.js
@@ -27,6 +27,48 @@ app.get("/v1/profit", function (request, response) {
 
 });
 
+let getCurrentRatePromise = function (coinName, investmentAmount , date ) {
+
+  var currentRateOptions = {
+    method: 'GET',
+    uri: 'https://rest.coinapi.io/v1/exchangerate/BTC/USD',
+    headers: {
+        'User-Agent': 'Request-Promise',
+        'X-CoinAPI-Key': API_KEY
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  return rp(currentRateOptions)
+    .then(function (data) {
+          return Math.trunc(data.rate);
+    })
+  .catch(function (err) {
+        console.log(err);
+    });
+
+};
+
+let getHistoryRatePromise = function (coinName, investmentAmount , date ) {
+    var historyRateOptions = {
+    method: 'GET',
+    uri: 'https://rest.coinapi.io/v1/trades/BITSTAMP_SPOT_BTC_USD/history?time_start=2016-01-01T00:00:00&limit=1',
+    headers: {
+        'User-Agent': 'Request-Promise',
+        'X-CoinAPI-Key': API_KEY
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  return rp(historyRateOptions)
+    .then(function (data) {
+          return Math.trunc(data[0].price);
+    })
+  .catch(function (err) {
+        console.log(err);
+    });
+  };
+
 app.get("/v2/profit", function (request, response) {
 
   let requiredQueryNames = ['coinName', 'investmentAmount', 'date'];
@@ -39,7 +81,14 @@ app.get("/v2/profit", function (request, response) {
 
   var { coinName, investmentAmount, date } = request.query;
 
-  res.status(204).end();
+
+  Promise.all([getCurrentRatePromise(), getHistoryRatePromise()])
+    .then((values) => {
+      console.log('lol');
+      console.log(values);
+    });
+
+    response.status(204).end();
 });
 
 app.get('/coinNames', (req, res) => {
@@ -72,6 +121,41 @@ app.get('/coinNames', (req, res) => {
     .catch(function (err) {
         console.log(err);
     });
+
+});
+
+app.get('/symbols', (req, res) => {
+    let symbols = {};
+
+      var options = {
+        method: 'GET',
+        uri: 'https://rest.coinapi.io/v1/symbols',
+        headers: {
+            'User-Agent': 'Request-Promise',
+            'X-CoinAPI-Key': API_KEY
+        },
+        json: true // Automatically parses the JSON string in the response
+      };
+
+    let Ids = [];
+    rp(options)
+      .then(function (data) {
+          data.forEach( (symbol) => {
+
+            if(symbol.asset_id_base === 'BCH' && symbol.asset_id_quote === 'USD')
+            {
+              console.log(symbol.symbol_id);
+              Ids.push({ id: symbol.symbol_id, start: symbol.data_start, end: symbol.data_end });
+
+            }
+          });
+
+          //console.log(data);
+           return res.json(Ids);
+      })
+      .catch(function (err) {
+          console.log(err);
+      });
 
 });
 
