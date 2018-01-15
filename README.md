6b1ffedfb4137e02979443b6958e1ccfa552d039
Glitch (remorse) <none>
2018-01-15 18:20:14 +0000

    🔨💾 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# buyers-remorse-backend
-Back-end Repo for Buyer's Remorse
+Welcome to Buyer's Remorse
+=========================
+
+Find out more [about Glitch](https://glitch.com/about).
+
+
+Project Layout
+------------
+
+On the back-end,
+- your app starts at `server.js`
+- add frameworks and packages in `package.json`
+- safely store app secrets in `.env` (nobody can see this but you and people you invite)
+
+-------------------
+
+\ ゜o゜)ノ
---
+++ server.js
@@ -0,0 +1,81 @@
+// server.js
+// where your node app starts
+const rp = require('request-promise');
+
+// init project
+var express = require('express');
+var app = express();
+
+let API_KEY = process.env.API_KEY;
+
+app.get("/", function (request, response) {
+  response.send('Home');
+});
+
+app.get("/v1/profit", function (request, response) {
+  
+    let requiredQueryNames = ['coinName', 'investmentAmount', 'date'];
+  
+    for (name in requiredQueryNames){
+    if (!request.query[requiredQueryNames[name]]) {
+      return response.status(404).send('Missing query.');
+    }  
+  }
+  
+  var { coinName, investmentAmount, date } = request.query;
+  
+  return response.json({ coinName, investmentAmount, date});
+  
+});
+
+app.get("v2/profit", function (request, response) {
+  
+  let requiredQueryNames = ['coinName', 'investmentAmount', 'date'];
+  
+    for (name in requiredQueryNames){
+    if (!request.query[requiredQueryNames[name]]) {
+      return response.status(404).send('Missing query.');
+    }  
+  }
+  
+  var { coinName, investmentAmount, date } = request.query;
+   
+});
+
+app.get('/coinNames', (req, res) => {
+  let coinNames = {};
+  
+    var options = {
+      method: 'GET',
+      uri: 'https://rest.coinapi.io/v1/assets',
+      headers: {
+          'User-Agent': 'Request-Promise',
+          'X-CoinAPI-Key': API_KEY
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
+  
+  rp(options)
+    .then(function (data) {
+        data.forEach( (coin) => {
+          
+            coinNames[coin.name] = {
+              start: coin.data_start,
+              end: coin.data_end
+            }
+            //console.log(`${coin.name} ${coin.type_is_crypto==1 ? 'is cryptocurrency.' : 'is not a coin.'}`);
+        });
+    })
+    .then( () => {
+      res.json(coinNames);
+  })
+    .catch(function (err) {
+        console.log(err);
+    });
+  
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
