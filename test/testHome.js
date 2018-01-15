3049a55dc7fd8f9604c64edfb08e447ac12193e5
Vernon Mensah <vernonmensah@gmail.com>
2018-01-15 15:31:23 -0500

    Another test for another path

--- test/testHome.js
+++ test/testHome.js
@@ -1,18 +1,30 @@
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 
+const app = require('../server');
+
 const should = chai.should();
-const app  = require('../server');
-
-
 chai.use(chaiHTTP);
 
 describe('GET / HOME ENDPOINT', () => {
   it('should return a status of 200', () => {
-    chai.request(app)
+    return chai.request(app)
       .get('/')
       .then( (response) => {
+        console.log(response);
         response.should.have.status(200);
+      });
+  })
+});
+
+describe('GET / INFO FROM COIN API ENDPOINT', () => {
+  it('should return coin names', () => {
+    return chai.request(app)
+      .get('/coinNames')
+      .then( (response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.a('Object');
 
       });
   })
