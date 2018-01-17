8934efc02b7b355546cc9ed041633a19c63149c8
Vernon Mensah <vernonmensah@gmail.com>
2018-01-17 12:52:03 -0500

    Added paths layout

--- router/v3.js
+++ router/v3.js
@@ -4,8 +4,37 @@ const express = require('express');
 
 let v3Router = express.Router();
 
-v3Router.get('/first', (req, res) => {
+v3Router.get('/investments', (req, res) => {
   res.send('Home');
 });
 
+//get all investments for a specific coin
+v3Router.get('/investments/:coinName', (req, res) => {
+  res.send('Home');
+});
+
+//get a investment for a coinName
+v3Router.get('/investments/:coinName/:id', (req, res) => {
+  res.send('Home');
+});
+
+//create a investment; needs a query
+  v3Router.post('/investment', (req, res) => {
+    res.send('Home');
+  });
+
+//update a investment
+v3Router.put('/investments/:coinName/:id', (req, res) => {
+  res.send('Home');
+});
+
+//del a investment
+v3Router.delete('/investments/:coinName/:id', (req, res) => {
+  res.send('Home');
+});
+
+//delete all investments
+v3Router.delete('/investments', (req, res) => {
+  res.send('Home');
+});
 module.exports = v3Router ;
