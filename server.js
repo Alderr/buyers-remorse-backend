28331a3da4f640d1d15c7aeb8f7dd92f96e7e825
Vernon Mensah <vernonmensah@gmail.com>
2018-01-17 13:11:47 -0500

    Added mongoose, mongodb to tests + travis

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
+
 const rp = require('request-promise');
 const cors = require('cors');
-// init project
+const mongoose = require('mongoose');
 var express = require('express');
+
 var app = express();
 
 const API_KEY = process.env.API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
+const PORT = process.env.PORT;
+
 const v3Router = require('./router/v3');
 
 app.use(cors());
@@ -209,9 +214,51 @@ app.get('/symbols/:coinName', (req, res) => {
 app.use('*', function (req, res) {
     res.status(404).json({ message: 'Not Found' });
 });
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
 
-module.exports = app;
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
--- test/testPaths.js
+++ test/testPaths.js
@@ -1,12 +1,25 @@
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 
-const app = require('../server');
+const { runServer, closeServer, app } = require('../server');
+
+let TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+let PORT = process.env.PORT;
 
 const should = chai.should();
+
 chai.use(chaiHTTP);
 
 describe('GET / HOME ENDPOINT', () => {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL, PORT);
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
   it('should return a status of 200', () => {
     return chai.request(app)
       .get('/')
